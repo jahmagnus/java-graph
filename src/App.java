c843f722ef5de5dbacfd180a9407c4349f415d2f
jahmagnus <jahmagnus@gmail.com>
2022-06-10 13:14:51 +1000

    ...

--- src/App.java
+++ src/App.java
@@ -1,5 +1,21 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        char[] vertices = {'a', 'b', 'c', 'd'};
+
+        Graph graph = new Graph(4);
+
+        for(Character element: vertices){
+            graph.addVertex(element);
+        }
+        
+        graph.addEdge('a', 'b');
+        graph.addEdge('a', 'c');
+        graph.addEdge('b', 'c');
+        graph.addEdge('c', 'd');
+        
+
+        graph.printGraph();
+
     }
 }
