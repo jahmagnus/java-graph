732409ca12255554d5bb4b6a3ccd3960379d8142
jahmagnus <jahmagnus@gmail.com>
2022-06-10 12:36:44 +1000

    ...

---
+++ src/Graph.java
@@ -0,0 +1,27 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+
+public class Graph {
+    
+    int numberOfVertices;
+    HashMap<Character, ArrayList<Character>> adjList;
+
+
+    public Graph(int num){
+        this.numberOfVertices = num;
+    
+    }
+
+    //add vertex
+    public void addVertex(char vertex){
+    
+    }
+
+
+    //add edge
+
+    //print graph
+
+}
